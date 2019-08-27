780d7a7767e8a8631bf597ff67d4b676ddef070a
Luan Castheloge <luanccp@gmail.com>
2019-08-26 21:41:31 -0300

    [ADD] New components and ways to use

--- my-first-project/src/app/server/server.component.ts
+++ my-first-project/src/app/server/server.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // selector: 'app-server',
-  selector: '.app-server',
+  selector: 'app-server',
+  // selector: '.app-server',
   templateUrl: './server.component.html'
 })
 export class ServerComponent{
+  serverId: number = 10
+  serverStatus: string = 'offline'
 
+  getString(){
+    return this.serverStatus;
+  }
 }
