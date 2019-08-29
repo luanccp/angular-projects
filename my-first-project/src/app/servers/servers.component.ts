06f3c95125b4a99f8cd148ce1d621545165037a7
Luan Castheloge <luanccp@gmail.com>
2019-08-29 20:41:50 -0300

    Start with directives

--- my-first-project/src/app/servers/servers.component.ts
+++ my-first-project/src/app/servers/servers.component.ts
@@ -6,24 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
+  serverCreated = false
   allowNewServer = false
   serverCreationStatus = 'No server was created!'
   serverName = ''
   constructor() {
-    setTimeout(()=>{
+    setTimeout(() => {
       this.allowNewServer = true
-    },2000)
+    }, 2000)
 
   }
 
   ngOnInit() {
   }
 
-  onCreateServer(){
+  onCreateServer() {
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName
+    this.serverCreated = true
   }
-  onUpdateServerName(event: Event){
+  onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value
   }
 }
