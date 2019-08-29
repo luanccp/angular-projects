f8df78133f781f0fb41805911943f2180a0aacd2
Luan Castheloge <luanccp@gmail.com>
2019-08-29 20:56:09 -0300

    ngstyle using property bind

--- my-first-project/src/app/server/server.component.ts
+++ my-first-project/src/app/server/server.component.ts
@@ -9,7 +9,15 @@ export class ServerComponent{
   serverId: number = 10
   serverStatus: string = 'offline'
 
+  constructor(){
+     this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
+  }
+
   getString(){
     return this.serverStatus;
+  }
+
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red'
   }
 }
