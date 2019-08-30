981422d81ca91f27ca70fa8a03a7bd7de8776729
Luan Castheloge <luanccp@gmail.com>
2019-08-29 21:05:42 -0300

    Criando varios com ngFor

--- my-first-project/src/app/servers/servers.component.ts
+++ my-first-project/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false
   serverCreationStatus = 'No server was created!'
   serverName = ''
+  servers = ['FileServer', 'DataServer']
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true
@@ -23,6 +24,7 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName
     this.serverCreated = true
+    this.servers.push(this.serverName)
   }
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value
