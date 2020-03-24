cb070190a041929a48b1f827a48be7fdb42624b5
Luan Castheloge <luanccp@gmail.com>
2020-03-24 19:39:58 -0300

    Rotas parametrizadas

--- pic-manager/front/src/app/photos/photo-list/photo-list.component.ts
+++ pic-manager/front/src/app/photos/photo-list/photo-list.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnInit } from "@angular/core";
+import { PhotoService } from "../photo/photo.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-photo-list',
-  templateUrl: './photo-list.component.html',
-  styleUrls: ['./photo-list.component.css']
+  selector: "app-photo-list",
+  templateUrl: "./photo-list.component.html",
+  styleUrls: ["./photo-list.component.css"]
 })
 export class PhotoListComponent implements OnInit {
-
   photos: Object[] = [];
 
-  constructor(private photoService: PhotoService) {}
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const userName = this.activatedRoute.snapshot.params.userName;
     this.photoService
-      .listFromUser("flavio")
+      .listFromUser(userName)
       .subscribe(photos => (this.photos = photos));
   }
-
 }
