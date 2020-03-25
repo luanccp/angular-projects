d2cea7420f63266bf49cee1c8b5349dd43e14a21
Luan Castheloge <luanccp@gmail.com>
2020-03-25 14:33:21 -0300

    Creating pipe and search. Creating resolve

--- pic-manager/front/src/app/photos/photo-list/photo-list.component.ts
+++ pic-manager/front/src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { PhotoService } from "../photo/photo.service";
 import { ActivatedRoute } from "@angular/router";
-import { Photo } from '../photo/photo';
+import { Photo } from "../photo/photo";
 
 @Component({
   selector: "app-photo-list",
@@ -10,16 +9,11 @@ import { Photo } from '../photo/photo';
 })
 export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
+  filter: string = "";
 
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName;
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => (this.photos = photos));
+    this.photos = this.activatedRoute.snapshot.data.photos;
   }
 }
