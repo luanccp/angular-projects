b999e20d24de9702b9ad8a0d446cb785e7d6ba6b
Luan Castheloge <luanccp@gmail.com>
2020-03-25 15:29:51 -0300

    Adding pagination

--- pic-manager/front/src/app/photos/photo-list/photo-list.component.ts
+++ pic-manager/front/src/app/photos/photo-list/photo-list.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from "@angular/router";
 import { Photo } from "../photo/photo";
 import { Subject } from 'rxjs';
 import { debounceTime } from "rxjs/operators";
+import { PhotoService } from '../photo/photo.service';
 @Component({
   selector: "app-photo-list",
   templateUrl: "./photo-list.component.html",
@@ -12,10 +13,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = "";
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService) {}
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName
     this.photos = this.activatedRoute.snapshot.data.photos;
     this.debounce
       .pipe(debounceTime(300))
@@ -25,5 +32,13 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load(){
+    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
+    .subscribe(photos => {
+      this.photos = this.photos.concat(photos);
+      if(!photos.length) this.hasMore= false;  
+    })
   }
 }
