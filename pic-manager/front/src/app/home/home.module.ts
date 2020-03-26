7080e89d667cb5b8ea9f06cba9410c9f5f9ffbdd
Luan Castheloge <luanccp@gmail.com>
2020-03-26 10:52:26 -0300

    transforming error message into a component

--- pic-manager/front/src/app/home/home.module.ts
+++ pic-manager/front/src/app/home/home.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from "@angular/core";
 import { SignInComponent } from "./signin/signin.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from '@angular/common';
+import { VMessageModule  } from "../shared/components/vmessage/vmessage.module";
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [ReactiveFormsModule, CommonModule]
+  imports: [
+    ReactiveFormsModule, 
+    CommonModule,
+    VMessageModule
+  ]
 })
 export class HomeModule {}
