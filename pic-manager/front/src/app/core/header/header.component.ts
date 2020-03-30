00660d6b04c34c3036d2b32d13b3bc71ea6dca60
Luan Castheloge <luanccp@gmail.com>
2020-03-30 12:34:24 -0300

    jwt decode and header decode

--- pic-manager/front/src/app/core/header/header.component.ts
+++ pic-manager/front/src/app/core/header/header.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { UserService } from "../user/user.service";
+import { Observable } from "rxjs";
+import { User } from "../user/user";
 
 @Component({
-    selector:'app-header',
-    templateUrl:'./header.component.html'
+  selector: "app-header",
+  templateUrl: "./header.component.html"
 })
 export class HeaderComponent {
+  user$: Observable<User>;
 
+  constructor(userService: UserService) {
+    this.user$ = userService.getUser();
+  }
 }
