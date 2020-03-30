c2682815cca5ef5a6b2252ba135915dd1a9376b3
Luan Castheloge <luanccp@gmail.com>
2020-03-30 12:39:34 -0300

    logout done

--- pic-manager/front/src/app/core/user/user.service.ts
+++ pic-manager/front/src/app/core/user/user.service.ts
@@ -23,4 +23,10 @@ export class UserService {
   getUser() {
       return this.userSubject.asObservable()
   }
+
+  logout(){
+     this.tokenService.removeToken()
+     this.userSubject.next(null)
+     
+  }
 }
