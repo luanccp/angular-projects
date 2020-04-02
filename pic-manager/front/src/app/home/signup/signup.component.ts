823048806a4cab5c38d00b4ededa41bc4f1eaa44
Luan Castheloge <luanccp@gmail.com>
2020-04-02 16:25:16 -0300

    Some validators on frontend

--- pic-manager/front/src/app/home/signup/signup.component.ts
+++ pic-manager/front/src/app/home/signup/signup.component.ts
@@ -1,6 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LowerCaseValidtator } from 'src/app/shared/validators/lower-case.validator';
 
 @Component({
-  templateUrl:'./signup.component.html'
+  templateUrl: "./signup.component.html"
 })
-export class SignUpComponent{}
+export class SignUpComponent implements OnInit {
+  signupForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+  
+  
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      email:['', [
+        Validators.required,
+        Validators.email,
+      ]],
+      userName:['', [
+        Validators.required,
+        LowerCaseValidtator
+      ]],
+      fullName:['', Validators.required],
+      password:['', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8)
+      ]]
+    }); 
+  }
+}
